a501f99dbb5a01bbbd6ebb7b4781f7095c102eda
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-06-28 20:39:19 -0300

    frontend

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import socket from './socket'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState([])
+  const [message, setMessage] = useState('')
+
+  const handleOnChangeMessage = (e) => {
+    e.preventDefault()
+    setMessage(e.target.value)
+  }
+
+  useEffect(() => {
+    socket.on('chat message', (msg) => {
+      setMessages([...messages, msg])
+    })
+  })
+
+  const sendMessage = () => {
+    socket.emit('chat message', message)
+    setMessage('')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <>
+        <h1>Home Page</h1>
+        <ul>
+            {messages.map((msg, index) => (
+                <li key={index}>{msg}</li>
+            ))}
+        </ul>
+        <input type="text" value={message} onChange={(e) => handleOnChangeMessage(e)}/>
+        <button onClick={sendMessage}>Send</button>
+      </>
   )
 }
 
