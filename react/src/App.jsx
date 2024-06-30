3e60d84ae3475cb65f0aae8d6e6b386ab79b9c32
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-06-29 21:08:46 -0300

    fixed socket comunication

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,35 +3,61 @@ import socket from './socket'
 import './App.css'
 
 function App() {
-  const [messages, setMessages] = useState([])
-  const [message, setMessage] = useState('')
-
-  const handleOnChangeMessage = (e) => {
-    e.preventDefault()
-    setMessage(e.target.value)
-  }
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
-    socket.on('chat message', (msg) => {
-      setMessages([...messages, msg])
-    })
-  })
+    socket.on("chat message", receiveMessage)
 
-  const sendMessage = () => {
-    socket.emit('chat message', message)
-    setMessage('')
+    return () => {
+      socket.off("chat message", receiveMessage);
+    };
+  }, []);
+
+  const receiveMessage = (message) =>
+    setMessages(state => [message, ...state]);
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newMessage = {
+      msg: message,
+      from: "Me",
+    };
+    setMessages(state => [newMessage, ...state]);
+    setMessage("");
+    socket.emit("chat message", newMessage.msg);
+  };
+
+  const handleOnChangeMessage = (e) => {
+    setMessage(e.target.value)
+
   }
-
   return (
       <>
-        <h1>Home Page</h1>
-        <ul>
-            {messages.map((msg, index) => (
-                <li key={index}>{msg}</li>
-            ))}
-        </ul>
-        <input type="text" value={message} onChange={(e) => handleOnChangeMessage(e)}/>
-        <button onClick={sendMessage}>Send</button>
+        <div className='container'>
+          <div className='front-camera-wrapper'>
+            <div className='front-camera'></div>
+            </div>
+
+          <div className='screen-wrapper'>
+            <div className='messages-wrapper'>
+              <ul className='messages-list'>
+                  {messages.map((msg, index) => (
+                    <div className='message-wrapper' key={index}>
+                      <li key={index} className={msg.from == 'Me' ? 'my-message': 'guest-message'}>{msg.from == 'Me' ? 'Me': 'Guest'}: {msg.msg}</li>
+                    </div>
+                  ))}
+              </ul>
+          </div>
+          <div className='form-wrapper'>
+            <form onSubmit={handleSubmit} className='form'>
+            <input type="text" value={message} onChange={(e) => handleOnChangeMessage(e)} placeholder='Type a message' />
+            <button type='submit' className='btn-send'>Send</button>
+            </form>
+          </div>
+          </div>
+        </div>
       </>
   )
 }
